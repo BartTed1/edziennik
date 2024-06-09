85e5e9bb0a6b57be8f4c3f5958e7fd85295d8634
Bartłomiej Teodorowicz <bart.ted.cont@gmail.com>
2024-06-09 14:11:26 +0200

    Student adding to class modal.

--- app/dziennik/klasy/klasy.tsx
+++ app/dziennik/klasy/klasy.tsx
@@ -4,10 +4,12 @@ import Class from "@/classes/Class";
 import User from "@/classes/User";
 
 export default function Klasy() {
-	const [classes, setClasses] = useState<Class[]>([])
+	const [classes, setClasses] = useState<Class[] | null>(null)
 	const [isChangeSupervisorModalOpen, setIsChangeSupervisorModalOpen] = useState(false)
 	const [selectedClassId, setSelectedClassId] = useState(0)
 	const [selectedClassName, setSelectedClassName] = useState("")
+	const [isAddStudentModalOpen, setIsAddStudentModalOpen] = useState(false)
+	const [isChangeClassNameModalOpen, setIsChangeClassNameModalOpen] = useState(false)
 
 	const fetchClasses = useCallback(async () => {
 		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/class`, {
@@ -36,10 +38,12 @@ export default function Klasy() {
 
 	return (
 		<div className={"container klasy"}>
-			<ChangeSupervisorModal isOpen={isChangeSupervisorModalOpen} setIsOpen={setIsChangeSupervisorModalOpen} classId={selectedClassId} className={selectedClassName}/>
+			<ChangeSupervisorModal isOpen={isChangeSupervisorModalOpen} setIsOpen={setIsChangeSupervisorModalOpen} classId={selectedClassId} className={selectedClassName} fetchClasses={fetchClasses}/>
+			<AddStudentModal isOpen={isAddStudentModalOpen} setIsOpen={setIsAddStudentModalOpen} classId={selectedClassId} fetchClasses={fetchClasses}/>
+			<ChangeClassNameModal isOpen={isChangeClassNameModalOpen} setIsOpen={setIsChangeClassNameModalOpen} classId={selectedClassId} className={selectedClassName} fetchClasses={fetchClasses}/>
 			<h1>Klasy</h1>
 			{
-				classes === null ? <h1>Trwa ładowanie...</h1> :
+				classes === null ? <p>Trwa ładowanie...</p> :
 					classes.length === 0 ? <p>Brak klas</p> :
 						classes.map((c, index) => (
 							<details key={index} className={"details"}>
@@ -60,6 +64,7 @@ export default function Klasy() {
 											<thead className={"table__tint"}>
 											<tr>
 												<th>Nr</th>
+												<th>ID</th>
 												<th>Imię ucznia</th>
 												<th>Nazwisko</th>
 											</tr>
@@ -69,8 +74,10 @@ export default function Klasy() {
 												c.students.map((s, index) => (
 													<tr key={index}>
 														<td>{index + 1}</td>
+														<td>{s.id}</td>
 														<td>{s.imie}</td>
 														<td>{s.nazwisko}</td>
+														<td>{s.birthDate}</td>
 													</tr>
 												))
 											}
@@ -84,13 +91,30 @@ export default function Klasy() {
 											setIsChangeSupervisorModalOpen(true)
 										}}
 										>Zmień wychowawcę</button>
-										<button className="button">Dodaj ucznia</button>
+										<button className="button" onClick={
+											_ => {
+												setSelectedClassId(c.id)
+												setIsAddStudentModalOpen(true)
+											}
+										}>Dodaj ucznia</button>
+										<button className="button" onClick={
+											_ => {
+												setSelectedClassId(c.id)
+												setSelectedClassName(c.name)
+												setIsChangeClassNameModalOpen(true)
+											}
+										}>Zmień nazwę</button>
 									</div>
 								</div>
 							</details>
 						)
 					)
 			}
+			<div className="button-set" style={{alignSelf: "flex-end"}}>
+				<button className="button">
+					Dodaj klasę
+				</button>
+			</div>
 		</div>
 	)
 }
@@ -99,16 +123,17 @@ interface ChangeSupervisorModalProps {
 	isOpen: boolean,
 	setIsOpen: (value: boolean) => void
 	classId: number
-	className: string
+	className: string,
+	fetchClasses: () => void
 }
 
-function ChangeSupervisorModal({isOpen, setIsOpen, classId, className}: ChangeSupervisorModalProps) {
+function ChangeSupervisorModal({isOpen, setIsOpen, classId, className, fetchClasses}: ChangeSupervisorModalProps) {
 	const dialogRef = useRef<HTMLDialogElement>(null)
 	const [teachers, setTeachers] = useState<User[] | null>(null)
 	const [selectedTeacherId, setSelectedTeacherId] = useState("")
 
 	const getTeachers = useCallback(async () => {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user?role=TEACHER`, {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user?role=TEACHER&page=0&size=200&sort=nazwisko,desc`, {
 			method: "GET",
 			headers: {
 				"Content-Type": "application/json",
@@ -144,6 +169,7 @@ function ChangeSupervisorModal({isOpen, setIsOpen, classId, className}: ChangeSu
 			console.error("Error changing supervisor")
 			return
 		}
+		fetchClasses()
 		setIsOpen(false)
 		setSelectedTeacherId("")
 	}
@@ -177,7 +203,7 @@ function ChangeSupervisorModal({isOpen, setIsOpen, classId, className}: ChangeSu
 			</div>
 			<form>
 				{
-					teachers === null ? <h1>Trwa ładowanie...</h1> :
+					teachers === null ? <p>Trwa ładowanie...</p> :
 						teachers.length === 0 ? <p>Brak dostępnych nauczycieli</p> : (
 							<div className="button-set w100">
 								<div className={"w100"}>
@@ -196,6 +222,217 @@ function ChangeSupervisorModal({isOpen, setIsOpen, classId, className}: ChangeSu
 				}
 			</form>
 			<button className={"button primary"} disabled={teachers === null} type={"submit"} onClick={changeSupervisior}>Zapisz</button>
+		</dialog>
+	)
+}
+
+interface AddStudentModalProps {
+	isOpen: boolean,
+	setIsOpen: (value: boolean) => void
+	classId: number,
+	fetchClasses: () => void
+}
+
+function AddStudentModal({isOpen, setIsOpen, classId, fetchClasses}: AddStudentModalProps) {
+	const dialogRef = useRef<HTMLDialogElement>(null)
+	const [students, setStudents] = useState<User[] | null>(null)
+	const [selectedStudentId, setSelectedStudentId] = useState<string>("")
+	const [isSubmitLocked, setIsSubmitLocked] = useState(false)
+
+	const getStudents = useCallback(async () => {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user?role=STUDENT`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${localStorage.getItem("token")}`
+			}
+		})
+		if (!response.ok) {
+			console.error("Error fetching students")
+			return
+		}
+		const data = await response.json()
+		if (!data) {
+			console.error("Error fetching students")
+			return
+		}
+		const studentsWithoutClass = data.content.filter((s: User) => s.studentclass === null)
+		setStudents(studentsWithoutClass)
+	}, [])
+
+	useEffect(() => {
+		if (!dialogRef.current) return
+		if (isOpen) {
+			dialogRef.current.showModal()
+			getStudents()
+		} else {
+			dialogRef.current.close()
+		}
+	}, [isOpen]);
+
+	const addStudent = async () => {
+		setIsSubmitLocked(true)
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/class/${classId}/students`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${localStorage.getItem("token")}`
+			},
+			body: JSON.stringify([{
+				id: selectedStudentId
+			}])
+		})
+		if (!response.ok) {
+			console.error("Error adding student")
+			setIsSubmitLocked(false)
+			return
+		}
+		fetchClasses()
+		setIsOpen(false)
+		setStudents(null)
+		setSelectedStudentId("")
+		setIsSubmitLocked(false)
+	}
+
+	return (
+		<dialog className={"modal"} ref={dialogRef} data-opened={isOpen}>
+			<div className="modal__header">
+				<div>
+					<h2>Dodaj uczniów</h2>
+					<p>Aby przenieść przypisanego do klasy ucznia, skorzystaj z okna edycji ucznia w module klasy.</p>
+				</div>
+				<svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+						 onClick={() => setIsOpen(false)}>
+					<path
+						d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+						fill="#081234"/>
+				</svg>
+			</div>
+			<form>
+				{
+					students === null ? <p>Trwa ładowanie...</p> :
+						students.length === 0 ? <p>Brak dostępnych uczniów</p> : (
+							<div className="button-set w100">
+								<div className={"w100"}>
+									<label htmlFor="teacher">Nowy uczeń:</label>
+									<select className={"select only w100"} value={selectedStudentId}
+													onChange={e => setSelectedStudentId(e.target.value)}>
+										<option hidden disabled value="">Wybierz</option>
+										{
+											students.map((s, index) => (
+												<option key={index} value={s.id}>id: {s.id} {s.imie} {s.nazwisko}</option>
+											))
+										}
+									</select>
+								</div>
+							</div>
+						)
+				}
+			</form>
+			<button className={"button primary"} disabled={students === null || isSubmitLocked || selectedStudentId === ""} type={"submit"} onClick={addStudent}>Dodaj
+			</button>
+		</dialog>
+	)
+}
+
+interface ChangeClassNameModalProps {
+	isOpen: boolean,
+	setIsOpen: (value: boolean) => void
+	classId: number,
+	className: string,
+	fetchClasses: () => void
+}
+
+function ChangeClassNameModal({isOpen, setIsOpen, classId, className, fetchClasses}: ChangeClassNameModalProps) {
+	const dialogRef = useRef<HTMLDialogElement>(null)
+	const [newClassName, setNewClassName] = useState(className)
+	const [isSubmitLocked, setIsSubmitLocked] = useState(false)
+	const [classSupervisor, setClassSupervisor] = useState<User | null>(null)
+
+	const getClass = useCallback(async () => {
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/class/${classId}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${localStorage.getItem("token")}`
+			},
+		})
+		if (!response.ok) {
+			console.error("Error fetching class")
+			return
+		}
+		const data = await response.json()
+		if (!data) {
+			console.error("Error fetching class")
+			return
+		}
+		setClassSupervisor(data.supervising)
+	}, [classId])
+
+	const changeClassName = async () => {
+		setIsSubmitLocked(true)
+		const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/class/${classId}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${localStorage.getItem("token")}`
+			},
+			body: JSON.stringify({
+				name: newClassName,
+				supervising: { id: classSupervisor?.id }
+			})
+		})
+		if (!response.ok) {
+			console.error("Error changing class name")
+			setIsSubmitLocked(false)
+			return
+		}
+		fetchClasses()
+		setIsOpen(false)
+		setIsSubmitLocked(false)
+	}
+
+	useEffect(() => {
+		if (!dialogRef.current) return
+		if (isOpen) {
+			dialogRef.current.showModal()
+			setNewClassName(className)
+			getClass()
+		} else {
+			dialogRef.current.close()
+		}
+	}, [isOpen]);
+
+	return (
+		<dialog className={"modal"} ref={dialogRef} data-opened={isOpen}>
+			<div className="modal__header">
+				<div>
+					<h2>Zmień nazwę klasy</h2>
+					<p>Wprowadź nową nazwę klasy.</p>
+				</div>
+				<svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+						 onClick={() => setIsOpen(false)}>
+					<path
+						d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+						fill="#081234"/>
+				</svg>
+			</div>
+			<form>
+				<div className="button-set w100">
+					<div className={"w100"}>
+						<label htmlFor="teacher">Nowa nazwa klasy:</label>
+						<input className={"input only w100"} type="text" value={newClassName} onChange={e => setNewClassName(e.target.value)}/>
+					</div>
+				</div>
+			</form>
+			<button
+				className={"button primary"}
+				disabled={isSubmitLocked || newClassName === "" || newClassName === className}
+				type={"submit"}
+				onClick={changeClassName}
+			>
+				Zapisz
+			</button>
 		</dialog>
 	)
 }
